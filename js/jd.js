191a46c315c48827beefe9cf175bf24639981117
敖武 <dongdong.fdd@alibaba-inc.com>
2019-09-07 23:25:19 +0800

    屏蔽京东二维码

--- js/jd.js
+++ js/jd.js
@@ -1,4 +1,5 @@
 
 setTimeout(function(){
   Array.prototype.slice.call(document.querySelector("div.login-form").querySelectorAll("div.login-tab")).pop().dispatchEvent(new Event('click'))
+  console.info('已关闭京东二维码登陆')
 },500)
